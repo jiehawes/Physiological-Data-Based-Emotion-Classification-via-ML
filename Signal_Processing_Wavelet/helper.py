f2c96ae556e351250b1e1bda3447dd955a563ea7
Jihwan Kim <39392336+raptorwoo@users.noreply.github.com>
2021-08-22 17:07:15 +0900

    modify decomposition tree data handling

---
+++ Signal_Processing_Wavelet/helper.py
@@ -0,0 +1,55 @@
+
+import matplotlib.pyplot as plt
+
+
+def zero_out_detailed(coeff, level):
+    for i in range(1,level+1):
+        for j in range(0, len(coeff[i])):
+            coeff[i][j] = 0
+
+
+def plot_two(data1, data2):
+    plt.subplot(121)
+    plt.plot(data1)
+    plt.title('gsr raw signal')
+    plt.subplot(122)
+    plt.plot(data2)
+    plt.title('reconstructed after level 10 decom')
+    plt.show()
+
+
+
+def plot_many(coeffs):
+    num = coeffs.size
+
+    ax1 = plt.subplot(241)
+    plt.title('A' + 10-num)
+    graph1 = plt.plot(coeffs[i])
+
+    ax2 = plt.subplot(242)
+    plt.title('D10')
+    graph2 = plt.plot(coeffs[1])
+
+    ax3 = plt.subplot(243)
+    plt.title('D9')
+    graph3 = plt.plot(coeffs[2])
+
+    ax4 = plt.subplot(244)
+    plt.title('D8')
+    graph4 = plt.plot(coeffs[3])
+
+    ax5 = plt.subplot(245)
+    plt.title('D7')
+    graph5 = plt.plot(coeffs[4])
+
+    ax6 = plt.subplot(246)
+    plt.title('D6')
+    graph6 = plt.plot(coeffs[5])
+
+    ax7 = plt.subplot(247)
+    plt.title('D5')
+    graph7 = plt.plot(coeffs[6])
+
+    ax8 = plt.subplot(248)
+    plt.title('D4')
+    graph8 = plt.plot(coeffs[7])
--- Signal_Processing_Wavelet/main.py
+++ Signal_Processing_Wavelet/main.py
@@ -5,6 +5,7 @@ from scipy import signal
 from scipy.interpolate import griddata
 from sklearn.preprocessing import minmax_scale
 import pywt
+import helper
 
 """ 
 @brief Slides with set window length to obtain amplitude and maximum value
@@ -21,15 +22,17 @@ def slide_window(n, win_len, step, arr):
         maximum_amplitude.append(max(arr[i:i+win_len]) - min(arr[i:i+win_len]))
     return maximum_value, maximum_amplitude
 
+plot_schedule_flag = True
+plot_show_flag = True
+export_data_flag = True
+
+# Iterate from File 1 to File 15
 for num in range(1, 16):
     read_path = '../Sensor_Data/GSR_30s_Sliced_Sub10/sliced_FS'
     file_number = '10e' + str(num)
     csv_file_extension = '.csv'
     png_file_extension = '.png'
     save_path = './Signal_Processing_Results/'
-    plot_schedule_flag = True
-    plot_show_flag = True
-    export_data_flag = True
 
     raw_data = pd.read_csv(read_path + file_number + csv_file_extension, sep=',', header=None)
     mother_wavelet = pywt.Wavelet('db3')
@@ -41,56 +44,36 @@ for num in range(1, 16):
         plt.savefig(save_path+file_number+'_raw_'+png_file_extension)
     if plot_show_flag:
         plt.show()
+
+    gsr_raw = raw_data.iloc[:,4]
+
     # A10 Coefficient
     coeffs_ten = pywt.wavedec(raw_data.iloc[:,4], mother_wavelet, level=10)
-    coeffs_eight =pywt.wavedec(raw_data.iloc[:,4], mother_wavelet, level=8)
-    coeffs_six =pywt.wavedec(raw_data.iloc[:,4], mother_wavelet, level=6)
+    helper.zero_out_detailed(coeffs_ten, 10)
+    recon_ten = pywt.waverec(coeffs_ten, 'db3')
 
-    print('size of A10 coeff:::', coeffs_ten[0].size,'size of raw coeff:::', raw_data.iloc[:,4].size)
+    # A8 Coefficient
+    coeffs_eight = pywt.wavedec(raw_data.iloc[:, 4], mother_wavelet, level=8)
+    helper.zero_out_detailed(coeffs_eight, 8)
+    recon_eight = pywt.waverec(coeffs_eight, 'db3')
 
-    # upsample coeffs_ten to have 1000 data points
-    raw_data = np.array(raw_data.iloc[:,4])
-    coeffs_ten = np.array(coeffs_ten[0])
-    coeffs_eight = np.array(coeffs_eight[0])
-    coeffs_six = np.array(coeffs_six[0])
+    # A6 Coefficient
+    coeffs_six = pywt.wavedec(raw_data.iloc[:, 4], mother_wavelet, level=6)
+    helper.zero_out_detailed(coeffs_six, 6)
+    recon_six = pywt.waverec(coeffs_six, 'db3')
 
-    print('A10 Size->',coeffs_ten.size,'///A8 Size->',coeffs_eight.size,'///A6 Size->', coeffs_six.size)
-
-    srate = 1
-    npnts = len(coeffs_ten)
-    timeA10 = np.arange(0, npnts) / srate
-    timeA8 = np.arange(0, npnts) / srate
-    timeA6 = np.arange(0, npnts) / srate
-
-    print('time:::', timeA10)
-
-    upsampleFactor = 1000 / len(coeffs_ten)
-    newNpnts = npnts * upsampleFactor
-    print('newNPnts:::', newNpnts)
-
-    newTime = np.arange(0, newNpnts) / (upsampleFactor * srate)
-
-    downdataRaw = signal.resample(raw_data, int(newNpnts))
-    updataA10 = griddata(timeA10, coeffs_ten, newTime, method='cubic')
-    updataA8 = griddata(timeA8, coeffs_eight, newTime, method='cubic')
-    updataA6 = griddata(timeA6, coeffs_six, newTime, method='cubic')
-
-    ## Cut the last few samples to match the data length of 1000
+    # Phasic = GSR - A10
     # SCVSR = A8 - A10
     # SCSR = A6 - A10
+    time_domain_size = min(len(gsr_raw), len(recon_ten), len(recon_eight), len(recon_six))
     phasic = []
     scvsr = []
     scsr = []
-    for i in range(0, 900):
-        phasic.append(downdataRaw[i] - updataA10[i])
-        scvsr.append(updataA8[i] - updataA10[i])
-        scsr.append(updataA6[i] - updataA10[i])
 
-    ## TODO:transform data back to time domain
-
-
-
-
+    for i in range(0, time_domain_size):
+        phasic.append(gsr_raw[i] - recon_ten[i])
+        scvsr.append(recon_eight[i] - recon_ten[i])
+        scsr.append(recon_six[i] - recon_ten[i])
 
     ## normalize extract parameters
     norm_phasic = minmax_scale(phasic)
@@ -121,7 +104,7 @@ for num in range(1, 16):
     rate_zero_cross_scvsr = zero_cross_scvsr.size
     rate_zero_cross_scsr = zero_cross_scsr.size
 
-    maximum_value, maximum_amplitude = slide_window(n=900, win_len=100, step=50, arr=phasic)
+    maximum_value, maximum_amplitude = slide_window(n=norm_phasic.size, win_len=100, step=50, arr=phasic)
     if plot_schedule_flag:
         plt.plot(maximum_amplitude)
         plt.plot(maximum_value)
